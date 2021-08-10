ec934747cf3bf987d7970f532ac7d6100e7069a1
Sachin Mane <61542725+rs-smane@users.noreply.github.com>
2021-08-10 23:37:38 +0530

    Update Transformer_handler_generalized.py

--- examples/Huggingface_Transformers/Transformer_handler_generalized.py
+++ examples/Huggingface_Transformers/Transformer_handler_generalized.py
@@ -3,13 +3,14 @@ import json
 import logging
 import os
 import ast
-import numpy as np
+import spacy
 import torch
 from transformers import AutoModelForSequenceClassification, AutoTokenizer, AutoModelForQuestionAnswering,AutoModelForTokenClassification
+
 from ts.torch_handler.base_handler import BaseHandler
-from transformers import BertTokenizer, BertConfig
 
 logger = logging.getLogger(__name__)
+
 
 class TransformersSeqClassifierHandler(BaseHandler, ABC):
     """
@@ -24,11 +25,13 @@ class TransformersSeqClassifierHandler(BaseHandler, ABC):
         properties = ctx.system_properties
         model_dir = properties.get("model_dir")
         serialized_file = self.manifest['model']['serializedFile']
+        self.nlp = spacy.load('en_core_web_sm')
         model_pt_path = os.path.join(model_dir, serialized_file)
-        self.device = torch.device("cuda:" + str(properties.get("gpu_id")))
-        logger.info("device : ", self.device) 
+        #self.device = torch.device("cuda:0")
+        self.device = torch.device("cuda:" + str(properties.get("gpu_id")) if torch.cuda.is_available() else "cpu")
         #read configs for the mode, model_name, etc. from setup_config.json
         setup_config_path = os.path.join(model_dir, "setup_config.json")
+
         if os.path.isfile(setup_config_path):
             with open(setup_config_path) as setup_config_file:
                 self.setup_config = json.load(setup_config_file)
@@ -51,12 +54,11 @@ class TransformersSeqClassifierHandler(BaseHandler, ABC):
         else:
             logger.warning('Missing the checkpoint or state_dict.')
 
-        if not os.path.isfile(os.path.join(model_dir, "vocab.*")):
+        if not os.path.isfile(os.path.join(model_dir, "vocab.txt")):
             self.tokenizer = AutoTokenizer.from_pretrained(self.setup_config["model_name"],do_lower_case=self.setup_config["do_lower_case"])
         else:
             self.tokenizer = AutoTokenizer.from_pretrained(model_dir,do_lower_case=self.setup_config["do_lower_case"])
 
-        #self.tokenizer = BertTokenizer.from_pretrained('bert-base-cased', do_lower_case=False)
         self.model.to(self.device)
         self.model.eval()
 
@@ -80,43 +82,83 @@ class TransformersSeqClassifierHandler(BaseHandler, ABC):
         text = data[0].get("data")
         if text is None:
             text = data[0].get("body")
-        input_text = text.decode('utf-8')
+        #text = str(text).replace("&nbsp", "")
+        input_texts = getSentences(self, text.lower().decode('utf-8').replace("&nbsp", ""))
         max_length = self.setup_config["max_length"]
-        logger.info("Received text: '%s'", input_text)
-        #preprocessing text for sequence_classification and token_classification.
-        if self.setup_config["mode"]== "sequence_classification" or self.setup_config["mode"]== "token_classification" :
-            inputs = self.tokenizer.encode_plus(input_text,max_length = int(max_length),pad_to_max_length = True, add_special_tokens = True, return_tensors = 'pt')
+        #logger.info("Received text: '%s'", text)
+        inputs_array = []
 
-        return inputs
+        
+        input_len = 0
+        sentences_value = ''
+        process_sentences = []
+        for sentence in input_texts:
+            input_len = input_len + len(sentence.split())
+            if input_len < int(max_length)-100:
+                sentences_value = sentences_value + ' ' + sentence
+            else:
+                process_sentences.append(sentences_value)
+                sentences_value = sentence
+                input_len = len(sentence.split())
 
-    def inference(self, inputs):
+        process_sentences.append(sentences_value)
+        logger.info("sentences '%s' ", process_sentences)
+
+        for input_text in process_sentences:
+            inputs = self.tokenizer.encode_plus(input_text,max_length = int(max_length), add_special_tokens = True, return_tensors = 'pt')
+            inputs_array.append(inputs)
+        #preprocessing text for question_answering.
+        return inputs_array
+
+    def inference(self, inputs_array):
         """ Predict the class (or classes) of the received text using the serialized transformers checkpoint.
         """
-
-
-        input_ids = inputs["input_ids"].to(self.device) #inputs["input_ids"].to(torch.device('cuda:0')) #inputs["input_ids"].to(self.device)
-        #logger.info("device : ", self.device)
-        
-        # Handling inference for token_classification.
-        if self.setup_config["mode"]== "token_classification":
-            with torch.no_grad():
+        prediction_array = []
+        for inputs in inputs_array:
+            input_ids = torch.tensor(inputs["input_ids"]).to(self.device)
+        # Handling inference for sequence_classification.
+            if self.setup_config["mode"]== "token_classification":
                 outputs = self.model(input_ids)[0]
-            #predictions = np.argmax(outputs.to('cpu').numpy(), axis=2)
-            predictions = torch.argmax(outputs, dim=2)
-            #logger.info("output : '%s'",predictions)
-            tokens = self.tokenizer.tokenize(self.tokenizer.decode(inputs["input_ids"][0]))
-            #tokens = self.tokenizer.tokenize(self.tokenizer.decode(inputs["input_ids"].to('cpu').numpy()[0]))
-            #logger.info("tokens : '%s'" , tokens)
-            #label_list = ''
-            if self.mapping:
-                label_list = self.mapping["label_list"]
-            label_list = label_list.strip('][').split(', ')
-            #logger.info("label list : '%s'",label_list)
-            prediction = [(token, label_list[prediction]) for token, prediction in zip(tokens, predictions[0].tolist())]
+                predictions = torch.argmax(outputs, dim=2)
+                tokens = self.tokenizer.tokenize(self.tokenizer.decode(inputs["input_ids"][0]))
+                if self.mapping:
+                    label_list = self.mapping["label_list"]
+                label_list = label_list.strip('][').split(', ')
+                prediction = [(token, label_list[prediction]) for token, prediction in zip(tokens, predictions[0].tolist())]
+                
+                prediction_array.extend(get_skill_list(prediction))
+                #skill_value_str = ''
+                #last_val = ''
+                #for value in prediction:
+                    #if 'SKILL' in value[1]:
+                        #if  "[SEP]" not in value[0]:
+                            #skill_value_str = skill_value_str + ' ' + value[0]
+                            #prediction_array.append(value[0])
+                    #elif skill_value_str and '##' in value[0]:
+                        #skill_value_str = skill_value_str + ' ' + value[0]
+                    #elif last_val != '' and 'SKILL' not in last_val[1] and 'I-SKILL' in value[1]:
+                        #skill_value_str = last_val[0] + ' ' + value[0]    
+                    #else:
+                        #if skill_value_str:    
+                            #prediction_array.append(skill_value_str.replace(' ##','').strip())
+                            #skill_value_str = ''
+                    #last_val = value        
+                    #else:
+                        #if skill_value_str and '##' in value[0]:
+                            #skill_value_str = skill_value_str + ' ' + value[0]
+                        #else:
+                            #if skill_value_str:    
+                                #prediction_array.append(skill_value_str.replace(' ##','').strip())
+                                #skill_value_str = ''    
+                                        
+                logger.info("Model predicted: '%s'", prediction)    
 
-            logger.info("Model predicted: '%s'", prediction)
-
-        return [prediction]
+        final_skill_array = []
+        for skill in prediction_array:
+            if skill not in final_skill_array:
+                final_skill_array.append(skill)
+        logger.info("length of Model predicted: '%s'", prediction_array)
+        return [final_skill_array]
 
     def postprocess(self, inference_output):
         # TODO: Add any needed post-processing of the model predictions here
@@ -125,6 +167,59 @@ class TransformersSeqClassifierHandler(BaseHandler, ABC):
 
 _service = TransformersSeqClassifierHandler()
 
+def getSentences(self, description):
+    sentences = []
+    text_sentences = self.nlp(description)
+    
+    for sentence in text_sentences.sents:
+        #print(sentence.text)
+        #logger.info("sentence text: '%s'", sentence.text)
+        sentences.append(sentence.text)
+    
+    return sentences
+
+def get_skill_list(predictions):
+    old_val = ''
+    complete_word = ''
+    complete_skill_word = ''
+    skill_list = []
+    for prediction in predictions:
+        if '##' in prediction[0]:
+            if complete_word :
+                complete_word = complete_word + prediction[0].replace('##','')
+            else:
+                complete_word = old_val[0] + prediction[0].replace('##','')
+        else:
+            #if complete_word :
+                #print('')#complete_word.replace('##',''))
+            complete_word = ''
+        if 'SKILL' in prediction[1]:
+            if 'B-SKILL' in prediction[1]:
+                if complete_word:
+                    complete_skill_word = complete_word
+                else:
+                    complete_skill_word = complete_skill_word + ' ' + prediction[0]
+                #print(complete_skill_word)
+            elif 'I-SKILL' in prediction[1] and 'SKILL' in old_val[1]:
+                if complete_word:
+                    #complete_skill_word = complete_skill_word + ' ' + complete_word
+                    if complete_word.count(complete_skill_word.split()[-1]):
+                        complete_skill_word = ' '.join(complete_skill_word.split(' ')[:-1]) + ' ' + complete_word
+                    else:
+                        complete_skill_word = complete_skill_word + ' ' + complete_word
+                else:
+                    complete_skill_word = complete_skill_word + ' ' + prediction[0]
+            elif 'I-SKILL' in prediction[1] and 'SKILL' not in old_val[1]:
+                if complete_word:
+                    complete_skill_word = old_val[0] + ' ' + complete_word
+                else:
+                    complete_skill_word = old_val[0] + ' ' + prediction[0]
+        else:
+            if complete_skill_word:
+                skill_list.append(complete_skill_word.strip())
+            complete_skill_word = ''    
+        old_val=prediction
+    return skill_list
 
 def handle(data, context):
     try:
