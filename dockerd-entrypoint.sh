600743d93f853a4d0d04ee0db104f5573e3077eb
Rohan Jadhav <86603506+rs-rjadhav@users.noreply.github.com>
2021-08-30 18:05:59 +0530

    Update dockerd-entrypoint.sh

--- dockerd-entrypoint.sh
+++ dockerd-entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -e
 
-# if [[ "$1" = "serve" ]]; then
-#     shift 1
-#     torchserve --start --model-store model-store --models my_tc=bert-ner.mar
-# else
-#     eval "$@"
-# fi
+if [[ "$1" = "serve" ]]; then
+   shift 1
+   torchserve --start --model-store model-store --models my_tc=bert-ner.mar
+else
+   eval "$@"
+fi
 
 # prevent docker exit
 tail -f /dev/null
